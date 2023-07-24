547bd83a4c078afba665d4aa4241ef1d21d6646b
Veerrohit Veeravadivel <veerrohitv08@gmail.com>
2023-07-24 16:46:06 +0530

    some tiny changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [image()]
 });
